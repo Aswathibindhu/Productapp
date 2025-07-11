25f85da9ec4216f64c7d05f33a7f4e72e2f124ac
Aswathibindhu <aswathibindhu4.com>
2025-07-11 11:40:53 +0530

    product

--- src/App.js
+++ src/App.js
@@ -4,20 +4,17 @@ import Addproduct from './Components/Addproduct';
 import Searchproduct from './Components/Searchproduct';
 import Deleteproduct from './Components/Deleteproduct';
 import ViewAllProducts from './Components/ViewAllProducts';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   return (
-    <div>
-
-      <Addproduct/>
-
-      <Searchproduct/>
-
-      <Deleteproduct/>
-      
-
-      <ViewAllProducts/>
-    </div>
+    <BrowserRouter>
+       <Routes>
+        <Route path="/" element={ <Addproduct/> } />
+        <Route path="/search" element={ <Searchproduct/> } />
+        <Route path="/delete" element={ <Deleteproduct/> } />
+        <Route path="/view" element={ <ViewAllProducts/> } />
+        </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/Components/Navigationbar.jsx
+++ src/Components/Navigationbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navigationbar = () => {
     return (
         <div>
 
-            <nav class="navbar navbar-expand-lg bg-primary">
+            <nav class="navbar navbar-expand-lg bg-danger">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="#">Product</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,16 +14,17 @@ const Navigationbar = () => {
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Add Product</a>
+                                <Link class="nav-link" to="/">Add Product</Link>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">View alla produtcs</a>
+                                <Link class="nav-link" to="/search">Search</Link>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link disabled" aria-disabled="true">Search Products</a>
+                                <Link class="nav-link" to="/view">View all produtcs</Link>
+
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link disabled" aria-disabled="true">Delete Products</a>
+                                <Link class="nav-link" to="/delete">Delete</Link>
                             </li>
                         </ul>
                     </div>
